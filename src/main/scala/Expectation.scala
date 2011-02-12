9937b030ded20ea20acb403e26ce793a1c5d10f6
Paul Butcher <paul@paulbutcher.com>
2011-02-12 16:30:17 +0000

    Back to a cliean sheet

--- src/main/scala/Expectation.scala
+++
@@ -1,9 +0,0 @@
-package com.borachio
-
-trait Expectation
-
-class Expectation2[T1, T2, R] extends Expectation {
-  
-  var expectedArguments: Option[Product2[T1, T2]] = None
-  var returnValue: R = _
-}
