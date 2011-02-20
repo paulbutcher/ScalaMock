f1d947b8f7b2c80ad49c5e93139dafe7fb942953
Paul Butcher <paul@paulbutcher.com>
2011-02-20 18:15:48 +0000

    Minor reformatting

--- src/main/scala/MockFunction.scala
+++ src/main/scala/MockFunction.scala
@@ -5,14 +5,20 @@ abstract class MockFunction(expectations: UnorderedExpectations) {
   protected def handle(arguments: Product) = expectations.handle(this, arguments)
 }
 
-class MockFunction0[R](expectations: UnorderedExpectations) extends MockFunction(expectations) with Function0[R] {
+class MockFunction0[R](expectations: UnorderedExpectations) 
+  extends MockFunction(expectations) with Function0[R] {
+
   def apply() = handle(None).asInstanceOf[R]
 }
 
-class MockFunction1[T1, R](expectations: UnorderedExpectations) extends MockFunction(expectations) with Function1[T1, R] {
+class MockFunction1[T1, R](expectations: UnorderedExpectations)
+  extends MockFunction(expectations) with Function1[T1, R] {
+
   def apply(v1: T1) = handle(new Tuple1(v1)).asInstanceOf[R]
 }
 
-class MockFunction2[T1, T2, R](expectations: UnorderedExpectations) extends MockFunction(expectations) with Function2[T1, T2, R] {
+class MockFunction2[T1, T2, R](expectations: UnorderedExpectations)
+  extends MockFunction(expectations) with Function2[T1, T2, R] {
+  
   def apply(v1: T1, v2: T2) = handle((v1, v2)).asInstanceOf[R]
 }
