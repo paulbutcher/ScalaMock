16a4accfb84e554c3785e97883762c7d4e68ed14
Paul Butcher <paul@paulbutcher.com>
2011-02-20 23:46:03 +0000

    Add further function arities

--- src/main/scala/MockFactory.scala
+++ src/main/scala/MockFactory.scala
@@ -36,6 +36,14 @@ trait MockFactory extends BeforeAndAfterEach { this: Suite =>
   protected def mockFunction[R] = new MockFunction0[R](expectations)
   protected def mockFunction[T1, R] = new MockFunction1[T1, R](expectations)
   protected def mockFunction[T1, T2, R] = new MockFunction2[T1, T2, R](expectations)
+  protected def mockFunction[T1, T2, T3, R] = new MockFunction3[T1, T2, T3, R](expectations)
+  protected def mockFunction[T1, T2, T3, T4, R] = new MockFunction4[T1, T2, T3, T4, R](expectations)
+  protected def mockFunction[T1, T2, T3, T4, T5, R] = new MockFunction5[T1, T2, T3, T4, T5, R](expectations)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, R] = new MockFunction6[T1, T2, T3, T4, T5, T6, R](expectations)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, R] = new MockFunction7[T1, T2, T3, T4, T5, T6, T7, R](expectations)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, R] = new MockFunction8[T1, T2, T3, T4, T5, T6, T7, T8, R](expectations)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, R] = new MockFunction9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](expectations)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R] = new MockFunction10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R](expectations)
   
   protected var autoVerify = true
   private var expectations: UnorderedExpectations = _
