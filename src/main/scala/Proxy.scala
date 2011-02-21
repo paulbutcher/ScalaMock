6ae45b82698ad32b7387ba3cd0835bab3f59d1de
Paul Butcher <paul@paulbutcher.com>
2011-02-21 01:04:29 +0000

    Add Proxy

---
+++ src/main/scala/Proxy.scala
@@ -0,0 +1,26 @@
+package com.borachio
+
+import java.lang.reflect.{InvocationHandler, Method, Proxy => JavaProxy}
+
+object Proxy {
+  
+  def interfacesFor(clazz: Class[_]): Array[Class[_]] = 
+    if (clazz.isInterface)
+      Array(clazz)
+    else 
+      clazz.getInterfaces
+
+  def create(classes: Class[_]*)(f: (Symbol, Array[AnyRef]) => AnyRef) = {
+    
+    val interfaces = for (clazz <- classes; interface <- interfacesFor(clazz)) yield interface
+
+    val classLoader = classes(0).getClassLoader
+
+    val handler = new InvocationHandler {
+      def invoke(proxy: AnyRef, method: Method, args: Array[AnyRef]) =
+        f(Symbol(method.getName), args)
+    }
+
+    JavaProxy.newProxyInstance(classLoader, interfaces.distinct.toArray, handler)
+  }
+}
---
+++ src/test/scala/ProxyTest.scala
@@ -0,0 +1,54 @@
+package com.borachio
+
+import org.scalatest.WordSpec
+
+trait Trait1 { def method1(x: Int, y: String): String }
+trait Trait2
+trait Trait3
+
+abstract class Class1 extends Trait1 with Trait2
+
+class ProxyTest extends WordSpec {
+  
+  def unimplemented(name: Symbol, args: Array[AnyRef]): AnyRef = error("unimplemented")
+  
+  "A proxy" should {
+    
+    "implement a single interface" in {
+      val p = Proxy.create(classOf[Trait1]) { unimplemented _ }
+      assert(p.isInstanceOf[Trait1])
+    }
+    
+    "implement multiple interfaces" in {
+      val p = Proxy.create(classOf[Trait1], classOf[Trait2], classOf[Trait3]) { unimplemented _ }
+      assert(p.isInstanceOf[Trait1])
+      assert(p.isInstanceOf[Trait2])
+      assert(p.isInstanceOf[Trait3])
+    }
+    
+    "implement all the interfaces of a class" in {
+      val p = Proxy.create(classOf[Class1]) { unimplemented _ }
+      assert(p.isInstanceOf[Trait1])
+      assert(p.isInstanceOf[Trait2])
+    }
+    
+    "implement a mixture of classes and interfaces" in {
+      val p = Proxy.create(classOf[Class1], classOf[Trait3]) { unimplemented _ }
+      assert(p.isInstanceOf[Trait1])
+      assert(p.isInstanceOf[Trait2])
+      assert(p.isInstanceOf[Trait3])
+    }
+    
+    "forward calls" in {
+      val p = Proxy.create(classOf[Trait1]) { (name: Symbol, args: Array[AnyRef]) =>
+        expect(name) { 'method1 }
+        expect(2) { args.length }
+        expect(42) { args(0).asInstanceOf[Int] }
+        expect("foo") { args(1).asInstanceOf[String] }
+        "called"
+      }.asInstanceOf[Trait1]
+
+      expect("called") { p.method1(42, "foo") }
+    }
+  }
+}
