ea7667b151404c04cf7e7c8ebea3327aee2a3c0b
Paul Butcher <paul@paulbutcher.com>
2011-02-21 01:47:12 +0000

    Add higher order function examples

---
+++ src/test/scala/examples/HigherOrderFunctions/HigherOrderFunctionsTest.scala
@@ -0,0 +1,32 @@
+package com.borachio.examples.mocksarentstubs
+
+import org.scalatest.Suite
+import com.borachio.MockFactory
+
+class HigherOrderFunctionsTest extends Suite with MockFactory {
+  
+  def testRepeat() {
+    def repeat(n: Int)(what: => Unit) {
+      for (i <- 0 until n)
+        what
+    }
+    
+    val f = mockFunction[Unit]
+    f expects () repeat 4
+    
+    repeat(4) { f() }
+  }
+  
+  def testFoldLeft() {
+    val f = mockFunction[String, Int, String]
+    
+    inSequence {
+      f expects ("zero", 1) returns "one" once;
+      f expects ("one", 2) returns "two" once;
+      f expects ("two", 3) returns "three" once;
+      f expects ("three", 4) returns "four" once;
+    }
+
+    expect("four") { Seq(1, 2, 3, 4).foldLeft("zero")(f) }
+  }
+}
