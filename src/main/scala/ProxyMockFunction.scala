b0ab8d76ffd0c4376d1ae297c8e1e5f6f63ece11
Paul Butcher <paul@paulbutcher.com>
2011-02-22 01:19:40 +0000

    Add additional ProxyMockFunction arities

--- src/main/scala/ProxyMockFunction.scala
+++ src/main/scala/ProxyMockFunction.scala
@@ -8,6 +8,14 @@ private[borachio] class ProxyMockFunction(expectations: UnorderedExpectations) e
     case 0 => None
     case 1 => new Tuple1(args(0))
     case 2 => (args(0), args(1))
+    case 3 => (args(0), args(1), args(2))
+    case 4 => (args(0), args(1), args(2), args(3))
+    case 5 => (args(0), args(1), args(2), args(3), args(4))
+    case 6 => (args(0), args(1), args(2), args(3), args(4), args(5))
+    case 7 => (args(0), args(1), args(2), args(3), args(4), args(5), args(6))
+    case 8 => (args(0), args(1), args(2), args(3), args(4), args(5), args(6), args(7))
+    case 9 => (args(0), args(1), args(2), args(3), args(4), args(5), args(6), args(7), args(8))
+    case 10 => (args(0), args(1), args(2), args(3), args(4), args(5), args(6), args(7), args(8), args(9))
     case _ => throw new IllegalArgumentException("ProxyMockFunction can't handle this many arguments")
   }
 }
