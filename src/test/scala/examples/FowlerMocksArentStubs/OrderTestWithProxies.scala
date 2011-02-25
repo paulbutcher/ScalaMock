9b655b4db27b03c64e905bc7a56507d3a55074f2
Paul Butcher <paul@paulbutcher.com>
2011-02-25 18:17:13 +0000

    Add license text and documentation

--- src/test/scala/examples/FowlerMocksArentStubs/OrderTestWithProxies.scala
+++ src/test/scala/examples/FowlerMocksArentStubs/OrderTestWithProxies.scala
@@ -1,8 +1,30 @@
+// Copyright (c) 2011 Paul Butcher
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package com.borachio.examples
 
 import org.scalatest.WordSpec
 import com.borachio.MockFactory
 
+// This is a reworked version of the example from Martin Fowler's article
+// Mocks Aren't Stubs http://martinfowler.com/articles/mocksArentStubs.html
 class OrderTestWithProxies extends WordSpec with MockFactory {
   
   "An order" when {
