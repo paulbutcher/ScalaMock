d887e9ed77b4219109d3e12d102f339d8bd9aa0a
Paul Butcher <paul@paulbutcher.com>
2011-02-26 16:52:13 +0000

    Add nexus publishing information

--- project/build/Borachio.scala
+++ project/build/Borachio.scala
@@ -2,4 +2,8 @@ import sbt._
 
 class Borachio(info: ProjectInfo) extends DefaultProject(info) { 
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
+  
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
