d741f3eadfafc0bac2465836f7525f277f8b25dc
Daniel Westheide <dwestheide@googlemail.com>
2011-05-07 22:08:30 +0200

    Added integration with specs2. This is achieved by a specs2 equivalent of the existing MockFactory traits. A specs2 version of the Fowler examples serves as a basic test of the integration and to demonstrate usage in specs2.

--- project/build/Borachio.scala
+++ project/build/Borachio.scala
@@ -3,8 +3,12 @@ import sbt._
 class Borachio(info: ProjectInfo) extends DefaultProject(info) { 
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
   val junit = "junit" % "junit" % "3.8.2"
+  val specs2 = "org.specs2" % "specs2_2.8.1" % "1.2"
   
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+  
+  def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
+  override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
 }
---
+++ src/test/scala/examples/FowlerMocksArentStubs/OrderSpecificationWithFunctions.scala
@@ -0,0 +1,32 @@
+package com.borachio.examples
+
+import org.specs2.mutable.Specification
+import com.borachio.specs2.MockFactory
+
+class OrderSpecificationWithFunctions extends Specification with MockFactory {
+  val hasInventoryMock = mockFunction[String, Int, Boolean]
+  val removeMock = mockFunction[String, Int, Unit]
+
+  val mockWarehouse = new Warehouse {
+    def hasInventory(product: String, quantity: Int) = hasInventoryMock(product, quantity)
+    def remove(product: String, quantity: Int) = removeMock(product, quantity)
+  }
+
+  "An order" should  {
+    "remove inventory when in stock" in {
+      inSequence {
+        hasInventoryMock expects ("Talisker", 50) returning true once;
+        removeMock expects ("Talisker", 50) once
+      }
+      val order = new Order("Talisker", 50)
+      order.fill(mockWarehouse)
+      order.isFilled must beTrue
+    }
+    "remove nothing when out of stock" in {
+      hasInventoryMock returns false once
+      val order = new Order("Talisker", 50)
+      order.fill(mockWarehouse)
+      order.isFilled must beFalse
+    }
+  }
+}
