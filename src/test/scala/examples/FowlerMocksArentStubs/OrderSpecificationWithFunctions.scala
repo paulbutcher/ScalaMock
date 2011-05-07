a0a19e2170735f384e59d0d0987d6cc288be444a
Daniel Westheide <dwestheide@googlemail.com>
2011-05-07 22:29:50 +0200

    Added missing comment

--- src/test/scala/examples/FowlerMocksArentStubs/OrderSpecificationWithFunctions.scala
+++ src/test/scala/examples/FowlerMocksArentStubs/OrderSpecificationWithFunctions.scala
@@ -3,6 +3,10 @@ package com.borachio.examples
 import org.specs2.mutable.Specification
 import com.borachio.specs2.MockFactory
 
+/**
+ * This is a demonstration and test of the Specs2 integration, using the example from
+ * Martin Fowler's article Mocks Aren't Stubs http://martinfowler.com/articles/mocksArentStubs.html
+ */
 class OrderSpecificationWithFunctions extends Specification with MockFactory {
   val hasInventoryMock = mockFunction[String, Int, Boolean]
   val removeMock = mockFunction[String, Int, Unit]
