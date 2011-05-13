4dd7cff871390d14ceffc3410b86043d131c3c8c
Paul Butcher <paul@paulbutcher.com>
2011-05-13 11:55:17 +0100

    Support for Scala 2.9.0

--- project/build/Borachio.scala
+++ project/build/Borachio.scala
@@ -1,9 +1,19 @@
 import sbt._ 
 
 class Borachio(info: ProjectInfo) extends DefaultProject(info) { 
-  val scalatest = "org.scalatest" % "scalatest" % "1.2"
+  val scalatest = 
+    buildScalaVersion match {
+      case "2.8.1" => "org.scalatest" % "scalatest" % "1.3"
+      case "2.9.0" => "org.scalatest" %% "scalatest" % "1.4.1"
+      case x => error("Unsupported Scala version " + x)
+    } 
   val junit = "junit" % "junit" % "3.8.2"
-  val specs2 = "org.specs2" % "specs2_2.8.1" % "1.2"
+  val specs2 = 
+    buildScalaVersion match {
+      case "2.8.1" => "org.specs2" %% "specs2" % "1.2"
+      case "2.9.0" => "org.specs2" %% "specs2" % "1.3"
+      case x => error("Unsupported Scala version " + x)
+    } 
   
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
