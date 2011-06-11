9f26c394dee9c5ab37565e83faf40cde3e169293
Paul Butcher <paul@paulbutcher.com>
2011-06-11 20:51:22 +0100

    Switch publishing credentials to snapshot repository

--- project/build/project.scala
+++ project/build/project.scala
@@ -2,6 +2,12 @@ import sbt._
 
 class Borachio(info: ProjectInfo) extends ParentProject(info) {
   
+  val scalaToolsSnapshots = ScalaToolsSnapshots
+
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+  
   lazy val library = project("library", "Borachio Library", new LibraryProject(_))
   lazy val plugin = project("plugin", "Borachio Plugin", new PluginProject(_), library)
   lazy val examples = project("examples", "Borachio Examples", new ExamplesProject(_), library, plugin)
@@ -11,10 +17,6 @@ class Borachio(info: ProjectInfo) extends ParentProject(info) {
     val scalatest = "org.scalatest" %% "scalatest" % "1.4.1" % "optional"
     val junit = "junit" % "junit" % "3.8.2" % "optional"
     val specs2 = "org.specs2" %% "specs2" % "1.3" % "optional"
-  
-    override def managedStyle = ManagedStyle.Maven
-    val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
-    Credentials(Path.userHome / ".ivy2" / ".credentials", log)
   }
   
   class PluginProject(info: ProjectInfo) extends DefaultProject(info)
