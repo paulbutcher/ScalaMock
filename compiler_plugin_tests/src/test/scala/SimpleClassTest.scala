ebc286ed960b4da2ddb33df00f4ce08ec6fb7ed6
Paul Butcher <paul@paulbutcher.com>
2011-06-27 19:00:36 +0100

    Add compiler_plugin_tests

---
+++ compiler_plugin_tests/src/test/scala/SimpleClassTest.scala
@@ -0,0 +1,43 @@
+// Copyright (c) 2011 Paul Butcher
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package com.borachio.plugin.test
+
+import org.scalatest.Suite
+import com.borachio.generated.GeneratedMockFactory
+import com.borachio.scalatest.MockFactory
+import java.net.URL
+
+// This is a reworked version of the example from Martin Fowler's article
+// Mocks Aren't Stubs http://martinfowler.com/articles/mocksArentStubs.html
+class SimpleClassTest extends Suite with MockFactory with GeneratedMockFactory {
+  
+  def mockClassPath = new URL("file:compiler_plugin_tests/target/scala_2.9.0/mock-classes/")
+  
+  def testSimpleClass {
+    val mockSimpleClass = mock[SimpleClass]
+    
+    mockSimpleClass.expects.nullMethod
+    mockSimpleClass.expects.methodWithOneArgument(42) returning "Expected return value"
+    
+    mockSimpleClass.nullMethod
+    expect("Expected return value") { mockSimpleClass.methodWithOneArgument(42) }
+  }
+}
