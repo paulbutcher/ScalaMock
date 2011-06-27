264601a7d613e5cecb7a94800b28fafb60c4f3e6
Paul Butcher <paul@paulbutcher.com>
2011-06-27 21:19:37 +0100

    Add class with non trivial constructor test

---
+++ compiler_plugin_tests/src/main/scala/ClassWithNonTrivialConstructor.scala
@@ -0,0 +1,35 @@
+// Copyright (c) 2011 Paul Butcher
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package com.borachio.plugin.test
+
+class ClassWithNonTrivialConstructor(arg1: Int) {
+  def nullMethod {}
+  def methodWithZeroArguments() = "methodWithZeroArguments: "+ arg1
+  final def methodWithOneArgument(x: Int) = "methodWithOneArgument: "+ x
+  def methodWithTwoArguments(x: Int, y: Int) = "methodWithTwoArguments: "+ (x, y)
+}
+
+object UsesClassWithNonTrivialConstructor {
+  def doSomething() = {
+    val x = new ClassWithNonTrivialConstructor(42)
+    x.methodWithZeroArguments()
+  }
+}
