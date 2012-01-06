46d81543ca017c1fadc2bdd1958753bbf02796b1
Paul Butcher <paul@paulbutcher.com>
2012-01-06 14:14:45 +0000

    Fix error messages for generated mock functions

--- core/src/main/scala/org/scalamock/MockFunction.scala
+++ core/src/main/scala/org/scalamock/MockFunction.scala
@@ -46,6 +46,9 @@ class MockFunction(protected val factory: MockFactoryBase, protected val name: S
   def toTypeSafeExpectation8[T1, T2, T3, T4, T5, T6, T7, T8, R] = factory.add(new TypeSafeExpectation8[T1, T2, T3, T4, T5, T6, T7, T8, R](this))
   def toTypeSafeExpectation9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R] = factory.add(new TypeSafeExpectation9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](this))
   def toTypeSafeExpectation10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R] = factory.add(new TypeSafeExpectation10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R](this))
+
+  // Can't extend NiceToString because that will mess up linearisation :-(
+  override def toString = name.name.toString
 }
 
 class MockFunction0[R](factory: MockFactoryBase, name: Symbol)
