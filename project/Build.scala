cebbb2e82c8bc0256c8df8089f5c3424e497d9cf
Paul Butcher <paul@paulbutcher.com>
2012-05-18 18:57:41 +0100

    Set version to 2.3

--- project/Build.scala
+++ project/Build.scala
@@ -29,7 +29,7 @@ object ScalaMockBuild extends Build {
   
   override lazy val settings = super.settings ++ Seq(
     organization := "org.scalamock",
-    version := "2.3-SNAPSHOT",
+    version := "2.3",
     crossScalaVersions := Seq("2.8.1", "2.8.2", "2.9.0", "2.9.0-1", "2.9.1", "2.9.2"),
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-Xfatal-warnings"),
 
