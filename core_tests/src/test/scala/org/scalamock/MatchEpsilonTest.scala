f2dc7d0c3fa5efdb094e037c34e80a364174960b
Paul Butcher <paul@paulbutcher.com>
2012-05-19 11:49:39 +0100

    MatchEpsilon

---
+++ core_tests/src/test/scala/org/scalamock/MatchEpsilonTest.scala
@@ -0,0 +1,43 @@
+// Copyright (c) 2011-2012 Paul Butcher
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package org.scalamock
+
+import org.scalatest.FreeSpec
+
+class MatchEpsilonTest extends FreeSpec {
+
+  "MatchEpsilon should" - {
+    "match anything that's close to the given value" in {
+      assert(new MatchEpsilon(1.0) == 1.0)
+      assert(new MatchEpsilon(1.0) == 1.0f)
+      assert(new MatchEpsilon(1.0) == 1.0001)
+      assert(new MatchEpsilon(1.0) == 1.0001f)
+      assert(new MatchEpsilon(1.0) == 1)
+    }
+    
+    "but not match anything that's not close enough" in {
+      assert(!(new MatchEpsilon(1.0) == 1.1))
+      assert(!(new MatchEpsilon(1.0) == 0.9))
+  
+      assert(!(new MatchEpsilon(1.0) == "foo"))
+    }
+  }
+}
