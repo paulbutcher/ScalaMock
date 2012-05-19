587aceb485ba1dc8e59fd60646fe2ade1eee6a61
Paul Butcher <paul@paulbutcher.com>
2012-05-19 02:02:44 +0100

    Skeleton project

---
+++ project/Build.scala
@@ -0,0 +1,66 @@
+// Copyright (c) 2011-2012 Paul Butcher
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import sbt._
+import Keys._
+
+object ScalaMockBuild extends Build {
+
+  override lazy val settings = super.settings ++ Seq(
+    organization := "org.scalamock",
+    version := "3.0-SNAPSHOT",
+    crossScalaVersions := Seq("2.10.0-M3"),
+    scalacOptions ++= Seq("-deprecation", "-unchecked", "-Xfatal-warnings"),
+
+    publishTo <<= version { v =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots") 
+      else
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomIncludeRepository := { _ => false },
+    publishArtifact in Test := false,
+    pomExtra := (
+      <url>http://paulbutcher.com/</url>
+      <licenses>
+        <license>
+          <name>BSD-style</name>
+          <url>http://www.opensource.org/licenses/bsd-license.php</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:paulbutcher/ScalaMock.git</url>
+        <connection>scm:git:git@github.com:paulbutcher/ScalaMock.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>paulbutcher</id>
+          <name>Paul Butcher</name>
+          <url>http://paulbutcher.com/</url>
+        </developer>
+      </developers>)
+  )
+
+  lazy val core = Project("core", file("core")) settings(
+    name := "ScalaMock Core"
+  )
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,7 @@
+resolvers += Classpaths.typesafeResolver
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.0.0")
+
+resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("com.jsuereth" % "xsbt-gpg-plugin" % "0.6")
