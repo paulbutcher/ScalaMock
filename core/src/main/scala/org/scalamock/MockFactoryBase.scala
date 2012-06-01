24b5f89eb43effd756159396d7508bd1cf7ee8eb
Paul Butcher <paul@paulbutcher.com>
2012-06-01 20:25:26 +0100

    Full multi-argument support

--- core/src/main/scala/org/scalamock/MockFactoryBase.scala
+++ core/src/main/scala/org/scalamock/MockFactoryBase.scala
@@ -53,25 +53,59 @@ trait MockFactoryBase extends Mock {
   protected implicit def functionName(name: Symbol) = FunctionName(name)
   protected implicit def functionName(name: String) = FunctionName(Symbol(name))
 
-  protected def mockFunction[R](name: FunctionName = FunctionName(Symbol("unnamed MockFunction0"))) = new MockFunction0[R](this, name.name)
+  protected def mockFunction[R](name: FunctionName) = new MockFunction0[R](this, name.name)
   protected def mockFunction[T1, R](name: FunctionName) = new MockFunction1[T1, R](this, name.name)
   protected def mockFunction[T1, T2, R](name: FunctionName) = new MockFunction2[T1, T2, R](this, name.name)
+  protected def mockFunction[T1, T2, T3, R](name: FunctionName) = new MockFunction3[T1, T2, T3, R](this, name.name)
+  protected def mockFunction[T1, T2, T3, T4, R](name: FunctionName) = new MockFunction4[T1, T2, T3, T4, R](this, name.name)
+  protected def mockFunction[T1, T2, T3, T4, T5, R](name: FunctionName) = new MockFunction5[T1, T2, T3, T4, T5, R](this, name.name)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, R](name: FunctionName) = new MockFunction6[T1, T2, T3, T4, T5, T6, R](this, name.name)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, R](name: FunctionName) = new MockFunction7[T1, T2, T3, T4, T5, T6, T7, R](this, name.name)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, R](name: FunctionName) = new MockFunction8[T1, T2, T3, T4, T5, T6, T7, T8, R](this, name.name)
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](name: FunctionName) = new MockFunction9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](this, name.name)
 
   protected def mockFunction[R] = new MockFunction0[R](this, Symbol("unnamed MockFunction0"))
   protected def mockFunction[T1, R] = new MockFunction1[T1, R](this, Symbol("unnamed MockFunction1"))
   protected def mockFunction[T1, T2, R] = new MockFunction2[T1, T2, R](this, Symbol("unnamed MockFunction2"))
+  protected def mockFunction[T1, T2, T3, R] = new MockFunction3[T1, T2, T3, R](this, Symbol("unnamed MockFunction3"))
+  protected def mockFunction[T1, T2, T3, T4, R] = new MockFunction4[T1, T2, T3, T4, R](this, Symbol("unnamed MockFunction4"))
+  protected def mockFunction[T1, T2, T3, T4, T5, R] = new MockFunction5[T1, T2, T3, T4, T5, R](this, Symbol("unnamed MockFunction5"))
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, R] = new MockFunction6[T1, T2, T3, T4, T5, T6, R](this, Symbol("unnamed MockFunction6"))
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, R] = new MockFunction7[T1, T2, T3, T4, T5, T6, T7, R](this, Symbol("unnamed MockFunction7"))
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, R] = new MockFunction8[T1, T2, T3, T4, T5, T6, T7, T8, R](this, Symbol("unnamed MockFunction8"))
+  protected def mockFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, R] = new MockFunction9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](this, Symbol("unnamed MockFunction9"))
 
   protected def stubFunction[R](name: FunctionName) = new StubFunction0[R](this, name.name)
   protected def stubFunction[T1, R](name: FunctionName) = new StubFunction1[T1, R](this, name.name)
   protected def stubFunction[T1, T2, R](name: FunctionName) = new StubFunction2[T1, T2, R](this, name.name)
+  protected def stubFunction[T1, T2, T3, R](name: FunctionName) = new StubFunction3[T1, T2, T3, R](this, name.name)
+  protected def stubFunction[T1, T2, T3, T4, R](name: FunctionName) = new StubFunction4[T1, T2, T3, T4, R](this, name.name)
+  protected def stubFunction[T1, T2, T3, T4, T5, R](name: FunctionName) = new StubFunction5[T1, T2, T3, T4, T5, R](this, name.name)
+  protected def stubFunction[T1, T2, T3, T4, T5, T6, R](name: FunctionName) = new StubFunction6[T1, T2, T3, T4, T5, T6, R](this, name.name)
+  protected def stubFunction[T1, T2, T3, T4, T5, T6, T7, R](name: FunctionName) = new StubFunction7[T1, T2, T3, T4, T5, T6, T7, R](this, name.name)
+  protected def stubFunction[T1, T2, T3, T4, T5, T6, T7, T8, R](name: FunctionName) = new StubFunction8[T1, T2, T3, T4, T5, T6, T7, T8, R](this, name.name)
+  protected def stubFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](name: FunctionName) = new StubFunction9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](this, name.name)
 
   protected def stubFunction[R] = new StubFunction0[R](this, Symbol("unnamed StubFunction0"))
   protected def stubFunction[T1, R] = new StubFunction1[T1, R](this, Symbol("unnamed StubFunction1"))
   protected def stubFunction[T1, T2, R] = new StubFunction2[T1, T2, R](this, Symbol("unnamed StubFunction2"))
+  protected def stubFunction[T1, T2, T3, R] = new StubFunction3[T1, T2, T3, R](this, Symbol("unnamed StubFunction3"))
+  protected def stubFunction[T1, T2, T3, T4, R] = new StubFunction4[T1, T2, T3, T4, R](this, Symbol("unnamed StubFunction4"))
+  protected def stubFunction[T1, T2, T3, T4, T5, R] = new StubFunction5[T1, T2, T3, T4, T5, R](this, Symbol("unnamed StubFunction5"))
+  protected def stubFunction[T1, T2, T3, T4, T5, T6, R] = new StubFunction6[T1, T2, T3, T4, T5, T6, R](this, Symbol("unnamed StubFunction6"))
+  protected def stubFunction[T1, T2, T3, T4, T5, T6, T7, R] = new StubFunction7[T1, T2, T3, T4, T5, T6, T7, R](this, Symbol("unnamed StubFunction7"))
+  protected def stubFunction[T1, T2, T3, T4, T5, T6, T7, T8, R] = new StubFunction8[T1, T2, T3, T4, T5, T6, T7, T8, R](this, Symbol("unnamed StubFunction8"))
+  protected def stubFunction[T1, T2, T3, T4, T5, T6, T7, T8, T9, R] = new StubFunction9[T1, T2, T3, T4, T5, T6, T7, T8, T9, R](this, Symbol("unnamed StubFunction9"))
 
   protected def where[T1](matcher: T1 => Boolean) = new FunctionAdapter1(matcher)
   protected def where[T1, T2](matcher: (T1, T2) => Boolean) = new FunctionAdapter2(matcher)
   protected def where[T1, T2, T3](matcher: (T1, T2, T3) => Boolean) = new FunctionAdapter3(matcher)
+  protected def where[T1, T2, T3, T4](matcher: (T1, T2, T3, T4) => Boolean) = new FunctionAdapter4(matcher)
+  protected def where[T1, T2, T3, T4, T5](matcher: (T1, T2, T3, T4, T5) => Boolean) = new FunctionAdapter5(matcher)
+  protected def where[T1, T2, T3, T4, T5, T6](matcher: (T1, T2, T3, T4, T5, T6) => Boolean) = new FunctionAdapter6(matcher)
+  protected def where[T1, T2, T3, T4, T5, T6, T7](matcher: (T1, T2, T3, T4, T5, T6, T7) => Boolean) = new FunctionAdapter7(matcher)
+  protected def where[T1, T2, T3, T4, T5, T6, T7, T8](matcher: (T1, T2, T3, T4, T5, T6, T7, T8) => Boolean) = new FunctionAdapter8(matcher)
+  protected def where[T1, T2, T3, T4, T5, T6, T7, T8, T9](matcher: (T1, T2, T3, T4, T5, T6, T7, T8, T9) => Boolean) = new FunctionAdapter9(matcher)
 
   protected def * = new MatchAny
 
@@ -126,6 +160,5 @@ trait MockFactoryBase extends Mock {
     private val log = new ListBuffer[Call]
   }
   
-//  private val callLog = new CallLog
   private var expectationContext: Handlers = _
 }
