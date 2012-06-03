ccc3f7e8f78ca1f41ac9433ac73263e7254dae2b
Paul Butcher <paul@paulbutcher.com>
2012-06-03 22:14:08 +0100

    Add ControllerTest

--- examples/src/main/scala/org/scalamock/examples/Controller.scala
+++ examples/src/main/scala/org/scalamock/examples/Controller.scala
@@ -20,12 +20,38 @@
 
 package org.scalamock.examples
 
-trait Turtle {
-  def penUp()
-  def penDown()
-  def forward(distance: Double)
-  def turn(angle: Double)
-  def getAngle: Double
-  def getPosition: (Double, Double)
-  def setPosition(x: Double, y: Double): (Double, Double)
+import scala.math.{atan2, sqrt}
+ 
+class Controller(turtle: Turtle) {
+ 
+  def drawLine(start: (Double, Double), end: (Double, Double)) {
+    moveTo(start)
+ 
+    val initialAngle = turtle.getAngle
+    val deltaPos = delta(start, end)
+ 
+    turtle.turn(angle(deltaPos) - initialAngle)
+    turtle.penDown
+    turtle.forward(distance(deltaPos))
+  }
+ 
+  def delta(pos1: (Double, Double), pos2: (Double, Double)) = 
+    (pos2._1 - pos1._1, pos2._2 - pos1._2)
+ 
+  def distance(delta: (Double, Double)) = 
+    sqrt(delta._1 * delta._1 + delta._2 * delta._2)
+ 
+  def angle(delta: (Double, Double)) = 
+    atan2(delta._2, delta._1)
+ 
+  def moveTo(pos: (Double, Double)) {
+    val initialPos = turtle.getPosition
+    val initialAngle = turtle.getAngle
+ 
+    val deltaPos = delta(initialPos, pos)
+ 
+    turtle.penUp
+    turtle.turn(angle(deltaPos) - initialAngle)
+    turtle.forward(distance(deltaPos))
+  }
 }
--- examples/src/test/scala/org/scalamock/examples/ControllerTest.scala
+++ examples/src/test/scala/org/scalamock/examples/ControllerTest.scala
@@ -20,12 +20,30 @@
 
 package org.scalamock.examples
 
-trait Turtle {
-  def penUp()
-  def penDown()
-  def forward(distance: Double)
-  def turn(angle: Double)
-  def getAngle: Double
-  def getPosition: (Double, Double)
-  def setPosition(x: Double, y: Double): (Double, Double)
+import org.scalatest.FunSuite
+import org.scalamock.scalatest.MockFactory
+import scala.math.{Pi, sqrt}
+ 
+class ControllerTest extends FunSuite with MockFactory {
+ 
+  test("draw line") {
+    val mockTurtle = mock[Turtle]
+    val controller = new Controller(mockTurtle)
+ 
+    inSequence {
+      inAnyOrder {
+        (mockTurtle.penUp _) expects ()
+        (mockTurtle.getPosition _).expects () returning (0.0, 0.0)
+        (mockTurtle.getAngle _).expects () returning 0.0
+      }
+      (mockTurtle.turn _) expects ~(Pi / 4)
+      (mockTurtle.forward _) expects ~sqrt(2.0)
+      (mockTurtle.getAngle _) expects () returning Pi / 4
+      (mockTurtle.turn _) expects ~(-Pi / 4)
+      (mockTurtle.penDown _) expects ()
+      (mockTurtle.forward _) expects 1.0
+    }
+ 
+    controller.drawLine((1.0, 1.0), (2.0, 1.0))
+  }
 }
