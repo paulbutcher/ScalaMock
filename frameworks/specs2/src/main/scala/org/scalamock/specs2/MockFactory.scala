8c56da4e7b195bf1d1c9f14e97f936c8aa97c34a
Paul Butcher <paul@paulbutcher.com>
2012-06-08 21:29:15 +0100

    Force specs2 tests using ScalaMock to execute sequentially

--- frameworks/specs2/src/main/scala/org/scalamock/specs2/MockFactory.scala
+++ frameworks/specs2/src/main/scala/org/scalamock/specs2/MockFactory.scala
@@ -1,13 +1,15 @@
 package org.scalamock.specs2
 
 import org.scalamock.MockFactoryBase
+import org.specs2.main.ArgumentsShortcuts
 import org.specs2.specification.{BeforeExample, AfterExample}
 
 /**
  * A trait that can be mixed into a [[http://etorreborre.github.com/specs2/ Specs2]] specification to provide
  * mocking support.
  */
-trait MockFactory extends MockFactoryBase with BeforeExample with AfterExample {
+trait MockFactory extends MockFactoryBase with BeforeExample with AfterExample { self: ArgumentsShortcuts =>
+  sequential
   protected def before = resetExpectations
   protected def after = if (autoverify) verifyExpectations
   protected var autoverify = true
