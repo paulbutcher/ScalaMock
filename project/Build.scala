8fd3c330fb7cf59a6b9fed7fa419651dd55b0e9a
Paul Butcher <paul@paulbutcher.com>
2012-06-29 23:27:29 +0100

    Switch back to SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -23,7 +23,7 @@ import Keys._
 import sbt.inc.Analysis
 
 object BuildSettings {
-  val buildVersion = "3.0-M1"
+  val buildVersion = "3.0-SNAPSHOT"
   val buildScalaVersion = "2.10.0-M4"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
