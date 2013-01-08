c874cac2beaeff100847640ea3c876d8252c31f2
Paul Butcher <paul@paulbutcher.com>
2013-01-08 18:41:44 +0000

    Revert "Update documentation to 3.0 release"
    This reverts commit 79bc22631210cd8faab7bbfd7344ac45394d317c.

--- api/lib/index.js
+++ api/lib/index.js
@@ -14,10 +14,7 @@ var title = $(document).attr('title');
 var lastHash = "";
 
 $(document).ready(function() {
-    $('body').layout({ 
-        west__size: '20%',
-        center__maskContents: true 
-    });
+    $('body').layout({ west__size: '20%' });
     $('#browser').layout({
         center__paneSelector: ".ui-west-center"
         //,center__initClosed:true
