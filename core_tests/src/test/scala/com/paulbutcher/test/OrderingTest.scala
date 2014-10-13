c470b7c5a846b7b1d279577dbb67a506612499f6
Paweł Wiejacha <pawel.wiejacha@gmail.com>
2014-10-13 23:45:34 +0200

    tests: reorganized OrderingTest

--- core_tests/src/test/scala/com/paulbutcher/test/OrderingTest.scala
+++ core_tests/src/test/scala/com/paulbutcher/test/OrderingTest.scala
@@ -20,178 +20,157 @@
 
 package com.paulbutcher.test
 
-import org.scalamock.scalatest.MockFactory
-import org.scalatest.FreeSpec
+class OrderingTest extends IsolatedSpec {
 
-class OrderingTest extends FreeSpec with MockFactory {
-  
   autoVerify = false
-  
-  def repeat(n: Int)(what: => Unit) {
-    for (i <- 0 until n)
-      what
+
+  behavior of "Mock function"
+
+  it should "handle a degenerate sequence" in withExpectations {
+    val m = mockFunction[Int, Int]
+    inSequence {
+      m.expects(42).returning(10)
+    }
+    assertResult(10) { m(42) }
   }
-  
-  "Mock functions should" - {
 
-    "handle a degenerate sequence" in {
-      withExpectations {
-        val m = mockFunction[Int, Int]
-        inSequence {
-          m.expects(42).returning(10)
-        }
-        assertResult(10) { m(42) }
-      }
+  it should "handle a sequence of calls" in withExpectations {
+    val m = mockFunction[Int, Int]
+    inSequence {
+      m.expects(42).returning(10).repeated(3 to 7)
+      m.expects(43).returning(11).once
+      m.expects(44).returning(12).twice
     }
-    
-    "handle a sequence of calls" in {
-      withExpectations {
-        val m = mockFunction[Int, Int]
-        inSequence {
-          m.expects(42).returning(10).repeated(3 to 7)
-          m.expects(43).returning(11).once
-          m.expects(44).returning(12).twice
-        }
-        repeat(5) { assertResult(10) { m(42) } }
-        repeat(1) { assertResult(11) { m(43) } }
-        repeat(2) { assertResult(12) { m(44) } }
-      }
-    }
-    
-    "fail if functions are called out of sequence" in {
-      intercept[ExpectationException](withExpectations {
-        val m = mockFunction[Int, Int]
-        inSequence {
-          m.expects(42).returning(10).repeated(3 to 7)
-          m.expects(43).returning(11).once
-          m.expects(44).returning(12).twice
-        }
-        repeat(5) { m(42) }
-      })
-    }
-    
-    "fail if the entire sequence isn't called" in {
-      intercept[ExpectationException](withExpectations {
-        val m = mockFunction[Int, Int]
-        inSequence {
-          m.expects(42).returning(10).repeated(3 to 7)
-          m.expects(43).returning(11).once
-          m.expects(44).returning(12).twice
-        }
-        repeat(5) { assertResult(10) { m(42) } }
-        repeat(1) { assertResult(11) { m(43) } }
-      })
-    }
-    
-    "not match a previous item in the sequence" in {
-      withExpectations {
-        val m = mockFunction[Int, Int]
-        inSequence {
-          m.expects(42).returning(10).anyNumberOfTimes
-          m.expects(43).returning(11).once
-        }
-        assertResult(10) { m(42) }
-        assertResult(11) { m(43) }
-        intercept[ExpectationException] { m(42) }
-      }
-    }
-    
-    "handle a combination of ordered and unordered expectations" in {
-      withExpectations {
-        val m = mockFunction[Int, Unit]
-  
-        m.expects(1)
-        inSequence {
-          m.expects(11)
-          m.expects(12)
-          m.expects(13)
-        }
-        m.expects(21)
-        inSequence {
-          m.expects(31)
-          m.expects(32)
-        }
-        m.expects(41)
-        
-        m(21)
-        m(31)
-        m(11)
-        m(12)
-        m(1)
-        m(32)
-        m(41)
-        m(13)
-      }
-    }
-    
-    "handle a sequence in which functions are called zero times" in {
-      withExpectations {
-        val m = mockFunction[Int, Unit]
-        inSequence {
-          m.expects(1).once
-          m.expects(2).never
-          m.expects(3).anyNumberOfTimes
-          m.expects(4).once
-        }
-        m(1)
-        m(4)
-      }
-    }
+    repeat(5) { assertResult(10) { m(42) } }
+    repeat(1) { assertResult(11) { m(43) } }
+    repeat(2) { assertResult(12) { m(44) } }
+  }
 
-    "handle valid deeply nested expectation contexts" in {
-      withExpectations {
-        val m = mockFunction[String, Unit]
-        
-        m.expects("1")
-        inSequence {
-          m.expects("2.1")
-          inAnyOrder {
-            m.expects("2.2.1")
-            inSequence {
-              m.expects("2.2.2.1")
-              m.expects("2.2.2.2")
-            }
-            m.expects("2.2.3").anyNumberOfTimes
-          }
-          m.expects("2.3")
-        }
-        m.expects("3")
-        
-        m("2.1")
-        m("1")
-        m("2.2.3")
-        m("2.2.2.1")
-        m("2.2.2.2")
-        m("2.2.1")
-        m("3")
-        m("2.2.3")
-        m("2.3")
+  it should "fail if functions are called out of sequence" in {
+    intercept[ExpectationException](withExpectations {
+      val m = mockFunction[Int, Int]
+      inSequence {
+        m.expects(42).returning(10).repeated(3 to 7)
+        m.expects(43).returning(11).once
+        m.expects(44).returning(12).twice
       }
+      repeat(5) { m(42) }
+    })
+  }
+
+  it should "fail if the entire sequence isn't called" in {
+    intercept[ExpectationException](withExpectations {
+      val m = mockFunction[Int, Int]
+      inSequence {
+        m.expects(42).returning(10).repeated(3 to 7)
+        m.expects(43).returning(11).once
+        m.expects(44).returning(12).twice
+      }
+      repeat(5) { assertResult(10) { m(42) } }
+      repeat(1) { assertResult(11) { m(43) } }
+    })
+  }
+
+  it should "not match a previous item in the sequence" in withExpectations {
+    val m = mockFunction[Int, Int]
+    inSequence {
+      m.expects(42).returning(10).anyNumberOfTimes
+      m.expects(43).returning(11).once
     }
-    
-    "handle invalid deeply nested expectation contexts" in {
-      intercept[ExpectationException](withExpectations {
-        val m = mockFunction[String, Unit]
-        
-        m.expects("1")
+    assertResult(10) { m(42) }
+    assertResult(11) { m(43) }
+    intercept[ExpectationException] { m(42) }
+  }
+
+  it should "handle a combination of ordered and unordered expectations" in withExpectations {
+    val m = mockFunction[Int, Unit]
+
+    m.expects(1)
+    inSequence {
+      m.expects(11)
+      m.expects(12)
+      m.expects(13)
+    }
+    m.expects(21)
+    inSequence {
+      m.expects(31)
+      m.expects(32)
+    }
+    m.expects(41)
+
+    m(21)
+    m(31)
+    m(11)
+    m(12)
+    m(1)
+    m(32)
+    m(41)
+    m(13)
+  }
+
+  it should "handle a sequence in which functions are called zero times" in withExpectations {
+    val m = mockFunction[Int, Unit]
+    inSequence {
+      m.expects(1).once
+      m.expects(2).never
+      m.expects(3).anyNumberOfTimes
+      m.expects(4).once
+    }
+    m(1)
+    m(4)
+  }
+
+  it should "handle valid deeply nested expectation contexts" in withExpectations {
+    val m = mockFunction[String, Unit]
+
+    m.expects("1")
+    inSequence {
+      m.expects("2.1")
+      inAnyOrder {
+        m.expects("2.2.1")
         inSequence {
-          m.expects("2.1")
-          inAnyOrder {
-            m.expects("2.2.1")
-            inSequence {
-              m.expects("2.2.2.1")
-              m.expects("2.2.2.2")
-            }
-            m.expects("2.2.3")
-          }
-          m.expects("2.3")
+          m.expects("2.2.2.1")
+          m.expects("2.2.2.2")
         }
-        m.expects("3")
-        
-        m("2.1")
-        m("1")
-        m("2.2.3")
-      })
+        m.expects("2.2.3").anyNumberOfTimes
+      }
+      m.expects("2.3")
     }
+    m.expects("3")
+
+    m("2.1")
+    m("1")
+    m("2.2.3")
+    m("2.2.2.1")
+    m("2.2.2.2")
+    m("2.2.1")
+    m("3")
+    m("2.2.3")
+    m("2.3")
+  }
+
+  it should "handle invalid deeply nested expectation contexts" in {
+    intercept[ExpectationException](withExpectations {
+      val m = mockFunction[String, Unit]
+
+      m.expects("1")
+      inSequence {
+        m.expects("2.1")
+        inAnyOrder {
+          m.expects("2.2.1")
+          inSequence {
+            m.expects("2.2.2.1")
+            m.expects("2.2.2.2")
+          }
+          m.expects("2.2.3")
+        }
+        m.expects("2.3")
+      }
+      m.expects("3")
+
+      m("2.1")
+      m("1")
+      m("2.2.3")
+    })
   }
 }
