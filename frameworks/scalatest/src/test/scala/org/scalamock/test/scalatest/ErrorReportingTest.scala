392c11f12bc652fc06d2deb3f9d0afcbb8d07d73
Paweł Wiejacha <pawel.wiejacha@gmail.com>
2014-11-13 20:34:20 +0100

    tests: test for issue #84

--- frameworks/scalatest/src/test/scala/org/scalamock/test/scalatest/ErrorReportingTest.scala
+++ frameworks/scalatest/src/test/scala/org/scalamock/test/scalatest/ErrorReportingTest.scala
@@ -30,31 +30,7 @@ import scala.language.postfixOps
 /**
  *  Tests that errors are reported correctly in ScalaTest suites
  */
-class ErrorReporting extends FlatSpec with ShouldMatchers {
-
-  /** Executes single ScalaTest test case and returns its outcome (i.e. either TestSucccess or TestFailure) */
-  def runTestCase[T <: Suite](suite: T): Event = {
-    class TestReporter extends Reporter {
-      var lastEvent: Option[Event] = None
-      override def apply(e: Event): Unit = { lastEvent = Some(e) }
-    }
-
-    val reporter = new TestReporter
-    suite.run(None, Args(reporter))
-    reporter.lastEvent.get
-  }
-
-  def getThrowable[ExnT <: Throwable](event: Event)(implicit m: Manifest[ExnT]): ExnT = {
-    event shouldBe a[TestFailed]
-
-    val testCaseError = event.asInstanceOf[TestFailed].throwable.get
-    testCaseError shouldBe a[ExnT]
-    testCaseError.asInstanceOf[ExnT]
-  }
-
-  def getErrorMessage[ExnT <: Throwable](event: Event)(implicit m: Manifest[ExnT]): String = {
-    getThrowable[ExnT](event).getMessage()
-  }
+class ErrorReporting extends FlatSpec with ShouldMatchers with TestSuiteRunner {
 
   "ScalaTest suite" should "report unexpected call correctly" in {
     class TestedSuite extends FunSuite with MockFactory {
---
+++ frameworks/scalatest/src/test/scala/org/scalamock/test/scalatest/StackableSuitesTest.scala
@@ -0,0 +1,65 @@
+// Copyright (c) 2011-2014 Paul Butcher
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package org.scalamock.test.scalatest
+
+import org.scalamock.scalatest.MockFactory
+import org.scalamock.test.mockable.TestTrait
+import org.scalatest._
+import org.scalatest.events.TestSucceeded
+
+class StackableSuitesTest extends FlatSpec with ShouldMatchers with TestSuiteRunner {
+
+  object EventLogger {
+    var events: List[String] = List.empty
+    def logEvent(event: String) = { events = events :+ event }
+  }
+
+  trait SuiteWrapper extends SuiteMixin { this: Suite =>
+    abstract override def withFixture(test: NoArgTest): Outcome = {
+      EventLogger.logEvent("SuiteWrapper setup")
+      val outcome = super.withFixture(test)
+      EventLogger.logEvent("SuiteWrapper cleanup")
+      outcome
+    }
+  }
+
+  class TestedSuite extends FunSuite with SuiteWrapper with MockFactory with ShouldMatchers {
+    test("execute block of code") {
+      val mockedTrait = mock[TestTrait]
+      (mockedTrait.oneParamMethod _).expects(1).onCall { arg: Int =>
+        EventLogger.logEvent("mock method called")
+        "one"
+      }
+
+      mockedTrait.oneParamMethod(1) shouldBe "one"
+    }
+  }
+
+  "ScalaTest suite" can "be mixed together with other traits which override withFixture" in {
+    val outcome = runTestCase[TestedSuite](new TestedSuite)
+    outcome shouldBe a[TestSucceeded]
+    EventLogger.events shouldBe List(
+      "SuiteWrapper setup",
+      "mock method called",
+      "SuiteWrapper cleanup")
+  }
+
+}
