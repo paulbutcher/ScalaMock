8642b01f8c1f632c5e276d17a888f67d228c5731
barkhorn <phil@faydark.co.uk>
2017-02-11 21:27:13 +0000

    added a mixin for using proxy and macro mocks in the same scalatest suite

---
+++ frameworks/scalatest/shared/src/main/scala/org/scalamock/scalatest/MixedMockFactory.scala
@@ -0,0 +1,22 @@
+package org.scalamock.scalatest
+
+import org.scalamock.clazz.{Mock => MacroMock}
+import org.scalamock.proxy.ProxyMockFactory
+
+import scala.reflect.ClassTag
+
+/**
+  * allows combining of macro mocks wih proxy mocks in the same Suite
+  * {{{
+  * val macroMock = mock[Foo]
+  * val proxyMock = Proxy.mock[Bar]
+  * }}}
+  */
+trait MixedMockFactory extends AbstractMockFactory with MacroMock {
+
+  object Proxy extends ProxyMockFactory {
+    import org.scalamock.proxy._
+    def mock[T: ClassTag]: T with Mock = super.mock[T]
+    def stub[T: ClassTag]: T with Stub = super.stub[T]
+  }
+}
---
+++ frameworks/scalatest/shared/src/test/scala/org/scalamock/test/scalatest/MixedMockFactoryTest.scala
@@ -0,0 +1,20 @@
+package org.scalamock.test.scalatest
+
+import org.scalamock.scalatest.MixedMockFactory
+import org.scalatest.{FlatSpec, Matchers}
+
+class MixedMockFactoryTest extends FlatSpec with MixedMockFactory with Matchers {
+  "mixed mocks" should "work" in {
+    trait Foo {
+      def getI: Int
+    }
+    val m = mock[Foo]
+    val p = Proxy.mock[Foo]
+
+    p.expects('getI)().returning(5).once()
+    m.getI _ expects() returning 42 once()
+
+    m.getI should be(42)
+    p.getI should be(5)
+  }
+}
