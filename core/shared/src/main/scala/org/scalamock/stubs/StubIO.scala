2b3c1f4d9903cc592440861dc6ddda065b563f57
Georgii Kovalev <goshacodes@gmail.com>
2025-01-09 15:22:01 +0500

    remove unused StubIO

--- core/shared/src/main/scala/org/scalamock/stubs/StubIO.scala
+++ core/shared/src/main/scala/org/scalamock/stubs/StubIO.scala
@@ -7,9 +7,3 @@ trait StubIO[F[+_, +_]]:
   
   def succeed[T](t: => T): F[Nothing, T]
   def flatMap[E, EE >: E, T, T2](fa: F[E, T])(f: T => F[EE, T2]): F[EE, T2]
-
-private[stubs]
-object StubIO:
-  trait Mono[F[+_]]:
-    def succeed[T](t: => T): F[T]
-    def flatMap[E, EE >: E, T, T2](fa: F[T])(f: T => F[T2]): F[T2]
